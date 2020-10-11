efc56a7c113a64e207f94aaba4b08ce56ba00810
Thomas Coradetti <tmcoradetti@gmail.com>
2020-10-11 19:27:10 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/nav';
 
 function App() {
   return (
     <div>
+      <Nav />
       <p>Hello!</p>
     </div>
   );
---
+++ src/components/nav/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a href="index.html" class="navbar-brand">Thomas Coradetti</a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarMenu">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a href="index.html" class="nav-link">About Me</a></li>
+                    <li class="nav-item"><a href="portfolio.html" class="nav-link">Portfolio</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
